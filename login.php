a6919c5ced56453e379dc4ad81c5ed79c5f3f4e1
KaidenESE <159060331+KaidenESE@users.noreply.github.com>
2026-06-19 11:58:19 -0400

    fixes

--- login.php
+++
@@ -1,8 +0,0 @@
-<?php
-echo "<h1>Login Information</h1>";
-
-echo "Username: " . $_POST["username"] . "<br>";
-echo "Password: " . $_POST["password"] . "<br>";
-
-echo '<p><a href="login.html">Back to Login</a></p>';
-?>
