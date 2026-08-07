48cacc497d4239c478c90dceb9dc50bce5cd1e0c
DonkeyPIECE <farhankhan621@gmail.com>
2026-08-06 21:00:38 -0400

    Update test_inheritance.php

--- php/test_inheritance.php
+++ php/test_inheritance.php
@@ -4,14 +4,12 @@ require_once 'oop/FloorNode.php';
 
 echo "<h2>Inheritance & Interface Verification Test</h2>";
 
-// Test FloorNode instantiation and inheritance
 $floorCall = new FloorNode(1, 2);
 $floorCall->handleInput('UP');
 echo "Floor Node ID: " . $floorCall->getNodeId() . "<br>";
 echo "Floor Number: " . $floorCall->getFloorNumber() . "<br>";
 echo "Up Button Requested: " . ($floorCall->isUpRequested() ? 'True' : 'False') . "<hr>";
 
-// Test Static Method/Property from Node class
 echo "<strong>Static Property Test:</strong><br>";
 echo "Total Nodes Created: " . Node::getTotalNodesCreated() . "<br>";
 
