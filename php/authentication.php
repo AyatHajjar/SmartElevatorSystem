f2d8653b0684ab422d22e198abdc0a896bab8295
DonkeyPIECE <farhankhan621@gmail.com>
2026-08-06 20:55:57 -0400

    Update authentication.php

--- php/authentication.php
+++ php/authentication.php
@@ -3,7 +3,6 @@
     
     $jsonFile = "../json/authorizedUsers.json";
     
-    // Write JSON
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         echo "<h1>Request Access Information</h1>";
 
@@ -17,11 +16,7 @@
             $fileContents = file_get_contents($jsonFile);
             $existingData = json_decode($fileContents, true) ?? [];
         }
-        //Below is if you want to new logins into the json
-        //file_put_contents($jsonFile, json_encode($existingData, JSON_PRETTY_PRINT));
-        
-        
-        // Check if user&pass is in database //////////////////////////////////////////////////
+       
         $accessGranted = false;
         foreach ($existingData as $userRecord) {
             if ($userRecord["username"] === $formData["username"] && $userRecord["password"] === $formData["password"]) {
@@ -30,7 +25,6 @@
             }
         }
         
-        // Display member link if access is granted
         if ($accessGranted) {
             echo '<p>Login Matches! You are a member</p>';
             echo '<p><a href="member.php">member</a></p>';
@@ -40,10 +34,7 @@
         } else {
             echo '<p>Access Denied: your not a member</p>';
         }
-        ///////////////////////////////////////////////////////////////////////////////
-       
         
-        // Read JSON
         if (file_exists($jsonFile)) {
             $loginJson = file_get_contents($jsonFile);
             $loginRequests = json_decode($loginJson, true) ?? [];
